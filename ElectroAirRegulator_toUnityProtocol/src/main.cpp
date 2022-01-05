74a8ca5a27dd2ac0d268e5e200e63ea27a8f2fcd
faruco10032 <T.kameoka10032@gmail.com>
2022-01-05 11:13:57 +0900

    add timer, read 3byte from Unity

--- ElectroAirRegulator_toUnityProtocol/src/main.cpp
+++ ElectroAirRegulator_toUnityProtocol/src/main.cpp
@@ -1,9 +1,55 @@
 #include <Arduino.h>
 
+#define RANGE 1 //目標気圧との誤差許容範囲
+
+//Timer関連セットアップ
+hw_timer_t * timer = NULL;
+//portMUX_TYPE型の変数で、メインループとISRで共用変数を変更する時の同期処理に使用
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+//メインループとISRで共用するためのカウンターです。コンパイラーの最適化によって消去されないように、volatile宣言をします
+volatile uint32_t isrCounter = 0;
+volatile uint32_t lastIsrAt = 0;
+
+// タイマー関数。割り込み処理用。
+void IRAM_ATTR onTimer(){
+  // Increment the counter and set the time of ISR
+  portENTER_CRITICAL_ISR(&timerMux);
+  isrCounter++;
+//  lastIsrAt = millis();
+  portEXIT_CRITICAL_ISR(&timerMux);
+
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  //timer set up
+  // Use 1st timer of 4 (counted from zero).
+  // Set 80 divider for prescaler (see ESP32 Technical Reference Manual for more　info).
+  timer = timerBegin(0, 80, true);
+  // Attach onTimer function to our timer.
+  timerAttachInterrupt(timer, &onTimer, true);
+  // Set alarm to call onTimer function every second (value in microseconds).
+  // Repeat the alarm (third parameter)
+  // timer 1ms
+  timerAlarmWrite(timer, 1000, true);
+  // Start an alarm
+  timerAlarmEnable(timer);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if ( Serial.available()>0) {
+
+      byte dataH = (byte)Serial.read();
+      byte dataL = (byte)Serial.read();
+
+      int data = (int) (
+            (((int)dataH << 6) & 0x000003FF)
+          | (((int)dataL << 0) & 0x0000003F)
+      );
+      while(Serial.read()!=0xff);
+
+    }
+
 }
